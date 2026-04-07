52c5d87163d65471b2d5b228cb542853ffc8f970
Luca Alessandri <alessandri.luca91@gmail.com>
2026-04-07 17:52:37 +0200

    Fix newline at end of run_jupyter.bat

--- run_jupyter.bat
+++ run_jupyter.bat
@@ -5,6 +5,7 @@ set IMAGE_NAME=ghcr.io/younginfolife/biohackathonbits2026:latest
 set SCRIPT_DIR=%~dp0
 
 docker run --rm -it ^
+  --platform linux/amd64 ^
   -p 8888:8888 ^
   -v "%SCRIPT_DIR%:/sharedFolder" ^
   %IMAGE_NAME%
