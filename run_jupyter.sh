b14b867cc588b09d68b6cf6d718907367667361f
Luca Alessandri <alessandri.luca91@gmail.com>
2026-04-07 17:49:34 +0200

    Add script to run Jupyter in Docker

---
+++ run_jupyter.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+IMAGE_NAME="ghcr.io/younginfolife/biohackathonbits2026:latest"
+
+docker run --rm -it \
+  -p 8888:8888 \
+  -v "${SCRIPT_DIR}:/notebooks" \
+  "${IMAGE_NAME}"
