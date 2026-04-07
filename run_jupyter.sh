562d9f6f00518feabe139bc8b47bf7f69b137be7
Luca Alessandri <alessandri.luca91@gmail.com>
2026-04-07 17:51:58 +0200

    Fix docker run command by adding newline

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 IMAGE_NAME="ghcr.io/younginfolife/biohackathonbits2026:latest"
 
 docker run --rm -it \
+  --platform linux/amd64 \
   -p 8888:8888 \
   -v "${SCRIPT_DIR}:/sharedFolder" \
   "${IMAGE_NAME}"
